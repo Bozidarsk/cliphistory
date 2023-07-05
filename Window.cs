20bf360b5cf8bee6a3757f1bdd83c87e63b50e9f
Bozidarsk <bozidarkabahcijski@gmail.com>
2023-07-05 17:48:50 +0300

    Fixed formatting.

--- Window.cs
+++ Window.cs
@@ -9,42 +9,42 @@ public static class Window
 	{
 		Application.Init();
 
-        Gtk.Window window = new Gtk.Window("");
-        window.SetDefaultSize(400, 450);
+		Gtk.Window window = new Gtk.Window("");
+		window.SetDefaultSize(400, 450);
 
-        CssProvider provider = new CssProvider();
-        provider.LoadFromData(System.IO.File.ReadAllText(Path.Css));
-        StyleContext.AddProviderForScreen(Gdk.Screen.Default, provider, 800);
+		CssProvider provider = new CssProvider();
+		provider.LoadFromData(System.IO.File.ReadAllText(Path.Css));
+		StyleContext.AddProviderForScreen(Gdk.Screen.Default, provider, 800);
 
-        VBox box = new VBox();
-        Viewport view = new Viewport();
-        ScrolledWindow scroll = new ScrolledWindow();
-        view.Add(box);
-        scroll.Add(view);
-        scroll.Name = "scroll";
-        box.Name = "box";
+		VBox box = new VBox();
+		Viewport view = new Viewport();
+		ScrolledWindow scroll = new ScrolledWindow();
+		view.Add(box);
+		scroll.Add(view);
+		scroll.Name = "scroll";
+		box.Name = "box";
 
-        for (int i = entries.Length - 1; i >= 0; i--) 
-        {
-            getProperties(entries[i], out byte[] content, out bool isImage, out bool isPinned, out string filename);
-            box.PackStart(new Entry(content, isImage, isPinned), false, false, 0);
-        }
+		for (int i = entries.Length - 1; i >= 0; i--) 
+		{
+			getProperties(entries[i], out byte[] content, out bool isImage, out bool isPinned, out string filename);
+			box.PackStart(new Entry(content, isImage, isPinned), false, false, 0);
+		}
 
-        if (entries.Length == 0) 
-        {
-        	box.PackStart(new Label("\udb80\udd4c"), false, false, 0);
-            box.PackStart(new Label("Clipboard is empty."), false, false, 0);
-            box.Children[0].Name = "empty-icon";
-            box.Children[1].Name = "empty-text";
-        }
+		if (entries.Length == 0) 
+		{
+			box.PackStart(new Label("\udb80\udd4c"), false, false, 0);
+			box.PackStart(new Label("Clipboard is empty."), false, false, 0);
+			box.Children[0].Name = "empty-icon";
+			box.Children[1].Name = "empty-text";
+		}
 
-        window.FocusOutEvent += (object sender, FocusOutEventArgs e) => { Application.Quit(); };
-        window.Resizable = false;
-        window.KeepAbove = true;
-        window.TypeHint = Gdk.WindowTypeHint.Menu;
-        window.Add(scroll);
-        window.ShowAll();
+		window.FocusOutEvent += (object sender, FocusOutEventArgs e) => { Application.Quit(); };
+		window.Resizable = false;
+		window.KeepAbove = true;
+		window.TypeHint = Gdk.WindowTypeHint.Menu;
+		window.Add(scroll);
+		window.ShowAll();
 
-        Application.Run();
+		Application.Run();
 	}
 }
