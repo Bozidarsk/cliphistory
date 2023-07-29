7d4ed9e62ba3534e340e85916df0ac8b957b57cf
bozidarsk <bozidarkabahcijski@gmail.com>
2023-07-29 12:16:57 +0300

    Added dependency list.

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+# build: mono, gtk-sharp-3
+# runtime: mono, gtk-sharp-3, gtk-layer-shell, wl-clipboard
+
 mcs *.cs -pkg:gtk-sharp-3.0 -out:cliphistory
 chmod 755 ./cliphistory
 sudo chown root:root ./cliphistory
